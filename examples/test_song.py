d821ba8fc4858fd40012826329a642dfb2c130db
Leo_org <leoart910@gmail.com>
2025-08-12 03:17:29 +0500

    Remove obsolete example scripts for video uploads from the repository.

--- examples/test_song.py
+++
@@ -1,9 +0,0 @@
-from tiktok_uploader.upload import upload_video
-
-upload_video(
-    "test.mp4",
-    description="!!!!",
-    song_name="тгк paranoyatg паранойя",
-    cookies="cookies.txt",
-    proxies="proxies.txt"
-)
